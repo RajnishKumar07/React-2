7a94a82d16fc610a8cb76a57b4b47b79cbffadb3
Rajnish Kumar <rajnish6633@gmail.com>
2026-07-09 17:47:21 +0530

    implement routing using react router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import { Routes, Route } from "react-router";
 import "./App.css";
 import { HomePage } from "./Pages/HomePage/HomePage";
 
 function App() {
-  return <HomePage></HomePage>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage></HomePage>}></Route>
+    </Routes>
+  );
 }
 
 export default App;
