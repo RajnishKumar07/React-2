9ca45553d2224e597fce69e2d948f2bebeb6bf63
Rajnish Kumar <rajnish6633@gmail.com>
2026-07-26 16:38:36 +0530

    Checkout page implemented

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router";
 import "./App.css";
 import { HomePage } from "./Pages/HomePage/HomePage";
+import CheckoutPage from "./Pages/CheckoutPage/CheckoutPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage></HomePage>}></Route>
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   );
 }
