ca6838fc7b05f105b6881535bd8ac71c3d2cda58
Rajnish Kumar <rajnish6633@gmail.com>
2026-07-26 17:01:06 +0530

    Order page imlemented

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router";
 import "./App.css";
 import { HomePage } from "./Pages/HomePage/HomePage";
 import CheckoutPage from "./Pages/CheckoutPage/CheckoutPage";
+import OrdersPage from "./Pages/OrdersPage/OrdersPage";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage></HomePage>}></Route>
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
     </Routes>
   );
 }
