c08480bb39ee12283e756612b6e4c6a2a87d8981
Rajnish Kumar <rajnish6633@gmail.com>
2026-07-28 12:11:53 +0530

    Separate into smaller component

---
+++ ecommerce-project/src/Pages/CheckoutPage/CheckoutHeader.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router";
+
+export function CheckoutHeader({ cart }) {
+  const totalQuantity = cart?.reduce((acc, item) => {
+    return acc + item.quantity;
+  }, 0);
+  return (
+    <div className="checkout-header">
+      <div className="header-content">
+        <div className="checkout-header-left-section">
+          <Link to="/">
+            <img className="logo" src="images/logo.png" />
+            <img className="mobile-logo" src="images/mobile-logo.png" />
+          </Link>
+        </div>
+
+        <div className="checkout-header-middle-section">
+          Checkout (
+          <Link className="return-to-home-link" to="/">
+            {totalQuantity} items
+          </Link>
+          )
+        </div>
+
+        <div className="checkout-header-right-section">
+          <img src="images/icons/checkout-lock-icon.png" />
+        </div>
+      </div>
+    </div>
+  );
+}
--- ecommerce-project/src/Pages/HomePage/HomePage.jsx
+++ ecommerce-project/src/Pages/HomePage/HomePage.jsx
@@ -2,8 +2,8 @@ import { Header } from "../../Components/Headers/Header";
 import axios from "axios";
 
 import "./HomePage.css";
-import { Fragment, useEffect, useState } from "react";
-import { formatMoney } from "../../utils/money";
+import { useEffect, useState } from "react";
+import { ProductGrid } from "./ProductGrid";
 export function HomePage({ cart }) {
   const [products, setProducts] = useState();
   useEffect(() => {
@@ -18,63 +18,7 @@ export function HomePage({ cart }) {
       <Header cart={cart}></Header>
 
       <div className="home-page">
-        <div className="products-grid">
-          {products?.map((product) => {
-            return (
-              <Fragment key={product.id}>
-                <div className="product-container">
-                  <div className="product-image-container">
-                    <img className="product-image" src={product.image} />
-                  </div>
-
-                  <div className="product-name limit-text-to-2-lines">
-                    {product.name}
-                  </div>
-
-                  <div className="product-rating-container">
-                    <img
-                      className="product-rating-stars"
-                      src={`images/ratings/rating-${product.rating.stars * 10}.png`}
-                    />
-                    <div className="product-rating-count link-primary">
-                      {product.rating.count}
-                    </div>
-                  </div>
-
-                  <div className="product-price">
-                    {formatMoney(product.priceCents)}$
-                  </div>
-
-                  <div className="product-quantity-container">
-                    <select>
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
-                      <option value="6">6</option>
-                      <option value="7">7</option>
-                      <option value="8">8</option>
-                      <option value="9">9</option>
-                      <option value="10">10</option>
-                    </select>
-                  </div>
-
-                  <div className="product-spacer"></div>
-
-                  <div className="added-to-cart">
-                    <img src="images/icons/checkmark.png" />
-                    Added
-                  </div>
-
-                  <button className="add-to-cart-button button-primary">
-                    Add to Cart
-                  </button>
-                </div>
-              </Fragment>
-            );
-          })}
-        </div>
+        <ProductGrid products={products}></ProductGrid>
       </div>
     </>
   );
